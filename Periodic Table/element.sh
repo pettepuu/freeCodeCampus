e3a15f9fc48081cac563807d8a4825d1d794b9e8
pettepuu <113012213+pettepuu@users.noreply.github.com>
2024-03-13 10:21:24 +0200

    Minor refactoring

--- Periodic Table/element.sh
+++ Periodic Table/element.sh
@@ -5,26 +5,24 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
-  exit
 fi
 
 #if argument is atomic number 
 if [[ $1 =~ ^[1-9]+$ ]]
 then
-  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
+  ELEMENT=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
 else
 #if argument is string
-  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1'")
+  ELEMENT=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1'")
 fi
 
 #element not in db
-if [[ -z $element ]]
+if [[ -z $ELEMENT ]]
 then
   echo "I could not find that element in the database."
-  exit
 fi
 
-echo $element | while IFS=" |" read an name symbol type mass mp bp 
+echo $ELEMENT | while IFS=" |" read AN NAME SYMBOL TYPE MASS MELTING_POINT BOILING_POINT 
 do
-  echo "The element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
+  echo "The element with atomic number $AN is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $name has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
 done
